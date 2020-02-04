f41021bf5ba3f3946ed14467d6e78039c24822db
Shitao Fan <43820194+ShitaoFan@users.noreply.github.com>
2020-02-04 16:14:21 +0000

    TestCommit

---
+++ Assets/March.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class March : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += Vector3.left; ;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += Vector3.right;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += Vector3.forward;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += Vector3.back;
+        }
+    }
+}
+
