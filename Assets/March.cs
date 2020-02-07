81e6793dd00bee0008c8bae71b2d9268930b0414
Shitao Fan <Shitao.Fan15@gmail.com>
2020-02-07 11:30:24 +0000

    Marching

--- Assets/March.cs
+++ Assets/March.cs
@@ -4,31 +4,62 @@ using UnityEngine;
 
 public class March : MonoBehaviour
 {
+    Animator leftLeg;
+    Animation march;
+
+    
     // Start is called before the first frame update
     void Start()
     {
+        leftLeg = GetComponent<Animator>();
+        march = GetComponent<Animation>();
         
+    }
+
+    void MarchFront()
+    {
+        if (Input.GetKey(KeyCode.A))
+        
+        {
+        leftLeg.SetBool("Front",true);
+        
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        
+        {
+        leftLeg.SetBool("Front",false);
+        
+        // march.enabled=false;
+        // leftLeg.GetComponent<Animator> ().Rebind ();
+        }
+    }
+    void MarchBack()
+    {
+        if (Input.GetKey(KeyCode.D))
+
+        {
+        
+        leftLeg.SetBool("Back",true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        
+        {
+        
+        leftLeg.SetBool("Back",false);
+        // march.Stop();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            transform.position += Vector3.left; ;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            transform.position += Vector3.right;
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            transform.position += Vector3.forward;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            transform.position += Vector3.back;
-        }
+        MarchFront();
+        {}
+        MarchBack();
     }
+
+        
 }
 
---
+++ Assets/SoldierMove.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierMove : MonoBehaviour
+{
+    public GameObject hit;
+    public GameObject soldier;
+    public float moveSpeed = 40.0f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        hit = GetComponentInChildren<GameObject>();
+        soldier = GetComponentInParent<GameObject>();
+        
+    }
+     void OnTriggerEnter(Collider hit) 
+    {
+        
+        Debug.Log("Hey");
+         soldier.transform.position += Vector3.forward*moveSpeed* Time.deltaTime;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+        
+        
+        // if (Input.GetKeyDown(KeyCode.A))
+        // {
+            
+        // }
+        // if (Input.GetKey(KeyCode.A))
+        // {
+            // transform.position += Vector3.forward * Time.deltaTime;
+        // }
+
+        // if (Input.GetKeyUp(KeyCode.A))
+        // {
+            
+        // }
+
+        // if (Input.GetKeyDown(KeyCode.D))
+        // {
+            // transform.position += Vector3.right;
+    //     }
+    //     if (Input.GetKeyDown(KeyCode.W))
+    //     {
+    //         transform.position += Vector3.forward;
+    //     }
+    //     if (Input.GetKeyDown(KeyCode.S))
+    //     {
+    //         transform.position += Vector3.back;
+    //     }
+    // }
+    }
+
+   
+}
