048e33eb0d73bf9062d9dcff0b4c36d049b75d45
Gretelxue <gretel@gmail.com>
2020-02-13 11:28:58 +0000

    2019.2.13.1

---
+++ Assets/Chenchen/Scripts/DrawLine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+    //private const float End = .45f;
+    private LineRenderer lineRenderer;
+    private float counter;
+    private float dist;
+
+    public Transform origin;
+    public Transform destination;
+    public float lineDrawSpeed = 6f;
+
+    public Transform[] Positions;
+    //public  bool sheCondition = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0,origin.position);
+ 
+        //dist = Vector3.Distance(origin.position ,destination .position );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if(sheCondition == true)
+        //{
+        //sheXian();
+        //}
+
+        lineRenderer.SetPosition(0, origin.position);
+        lineRenderer.SetPosition(1, destination.position);
+    }
+   
+}
