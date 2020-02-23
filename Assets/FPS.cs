2cc917077949fb1716f93a67a6bfb6ef9c7155df
Shitao Fan <Shitao.Fan15@gmail.com>
2020-02-23 23:56:19 +0000

    camera and control

--- Assets/FPS.cs
+++ Assets/FPS.cs
@@ -8,8 +8,47 @@ public class FPS : MonoBehaviour
    public Transform cam;
    float heading=0;
    Vector2  input;
+   Animator run;
+
+   void Update() 
+   {
+       heading += Input.GetAxis("Mouse X")*Time.deltaTime*45;
+       camPivot.rotation=Quaternion.Euler(0,heading,0);
+
+       input=new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+       input = Vector2.ClampMagnitude(input,1);
+      //  run.SetTrigger("RunFront");
+
+       Vector3 camF =cam.forward;
+       Vector3 camR= cam.right;
+
+       camF.y=0;
+       camR.y=0;
+       camF = camF.normalized;
+       camR=camR.normalized;
+
+       transform.position += (camF*input.y + camR*input.x)*Time.deltaTime*5;
+   }
 
 
+
+//    float translation;
+//    float strafe;
+//    public float rotationSpeed;
+
+   
+//      void FixedUpdate() 
+//        {
+//         Vector3 movement = new Vector3 (strafe, 0f, translation);
+
+//         transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (movement), rotationSpeed);    
+
+//         transform.Translate (cam.TransformDirection(movement));  
+//        }
+
+       
+
+      
 
 
 //    public float movementSpeed;
@@ -37,50 +76,6 @@ public class FPS : MonoBehaviour
 //           targetDirection = Camera.main.transform.TransformDirection(targetDirection);
 //           targetDirection.y = 0.0f;
 //      }
-
-
-
-
-
-
-
-//    float translation;
-//    float strafe;
-//    public float rotationSpeed;
-
-   
-//      void FixedUpdate() 
-//        {
-//         Vector3 movement = new Vector3 (strafe, 0f, translation);
-
-//         transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (movement), rotationSpeed);    
-
-//         transform.Translate (cam.TransformDirection(movement));  
-//        }
-
-
-
-   void Update() 
-   {
-       heading += Input.GetAxis("Mouse X")*Time.deltaTime*45;
-       camPivot.rotation=Quaternion.Euler(0,heading,0);
-
-       input=new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-       input = Vector2.ClampMagnitude(input,1);
-
-       Vector3 camF =cam.forward;
-       Vector3 camR= cam.right;
-
-       camF.y=0;
-       camR.y=0;
-       camF = camF.normalized;
-       camR=camR.normalized;
-
-
-    // transform.position+=new Vector3(input.x,0,input.y)*Time.deltaTime*5;
-       transform.position += (camF*input.y + camR*input.x)*Time.deltaTime*5;
-
-   }
    
    
    
