582ca68785a28463ec1a0f018489faef8034889d
YunHao-rgb <60631343+YunHao-rgb@users.noreply.github.com>
2020-02-27 13:58:42 +0000

    ddd

---
+++ Assets/YunHao/Script/CoorCubeMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class CoorCubeMove : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public PathCreator pathCreator2;
+    public float moveSpeed = 0.1f;
+    public float horizontalDistance;
+    public GameObject coorCube;
+    public Rigidbody cubeRigidbody;
+    public Animator charaAnimator;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalMove(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+    //VerticalMove(Input.GetAxis("Horizontal"));
+    }
+    void HorizontalMove(float horizontal, float vertical)
+    {
+        if(horizontal != 0)
+        {
+            charaAnimator.SetBool("Run", true);
+            horizontalDistance += moveSpeed * horizontal;
+            coorCube.transform.position = pathCreator.path.GetPointAtDistance(horizontalDistance);
+            coorCube.transform.rotation = pathCreator.path.GetRotationAtDistance(horizontalDistance);
+            if (horizontalDistance <= 0.2f )
+            {
+                horizontalDistance = 0.2f;
+            }
+                //cubeRigidbody.velocity = new Vector3(cubeRigidbody.velocity.x, cubeRigidbody.velocity.y, -horizontal * moveSpeed);
+                //Debug.Log("平移");
+        }
+        else
+        {
+            if(vertical == 0)
+            {
+                charaAnimator.SetBool("Run", false);
+                cubeRigidbody.velocity = Vector3.zero;
+            }
+        }
+    }
+}
---
+++ Assets/YunHao/Script/DoorTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    RotateAround RotateAround;
+    public bool triggerCondition = false;
+    public Animator charaAnimator;
+    void Start()
+    {
+        RotateAround = FindObjectOfType<RotateAround>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H) && triggerCondition == true)
+            {
+                Debug.Log("DoorTrigger");
+                RotateAround.Timetimetime = true;
+                charaAnimator.SetBool("PullTheDoor", true);
+            }
+        if (Input.GetKeyUp(KeyCode.H))
+        {
+            charaAnimator.SetBool("PullTheDoor", false);
+        }
+    }
+    void OnTriggerEnter(Collider collision) 
+    {
+        if(collision.tag == "Character")
+        {
+            triggerCondition = true;
+        }
+    }
+}
