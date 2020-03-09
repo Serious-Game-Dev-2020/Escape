3e1038e9fb862e39a6e3c91612c97912f9718702
Shitao Fan <Shitao.Fan15@gmail.com>
2020-03-09 22:56:45 +0000

    menuSetUp

---
+++ Assets/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+   public static GameManager instance = null;
+
+   public GameObject chapter1;
+   public GameObject chapter2;
+   public GameObject chapter3;
+   public GameObject chapter4;
+
+
+   enum Chapternumber{chapter1,chapter2,chapter3,chapter4}
+   Chapternumber chapter;
+
+   void Start()
+   {
+       chapter = Chapternumber.chapter1;
+       //change the transparance of the chapter picture
+   }
+   void Awake() 
+   {
+       if (instance == null){
+           instance = this;
+       }
+
+       else if (instance != this){
+
+           Destroy(gameObject);
+       }
+
+       DontDestroyOnLoad(gameObject);
+       Init();
+   } 
+
+   void Init(){
+       SceneManager.LoadScene("01Main");
+   }
+
+   
+
+   
+    
+}
---
+++ Assets/Scene/SceneLoad.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SceneLoad : MonoBehaviour
+{
+    GameManager gameManager;
+    void Start() {
+
+        gameManager = GameManager.instance;
+        ChapterChoose();
+    }
+    //condition to chapterchange
+    void OnMouseDown(){
+        ChapterChoose();
+    }
+    void ChapterChoose()
+   {
+       switch(gameManager.chapter){
+       case Chapternumber.chapter1:
+            GetComponent<Renderer>().material.color=chapter.chapter1;
+            break;
+       case Chapternumber.chapter2:
+
+            break;
+       case Chapternumber.chapter3:
+
+            break;
+       case Chapternumber.chapter4:
+
+            break;
+
+           
+       }
+   }
+    
+}
